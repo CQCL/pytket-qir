6db4cf042cab9ee197c0010abe506c61bd3c0fdc
Roland Guichard <roland.guichard@cambridgequantum.com>
2022-07-25 11:31:31 +0200

    Add changelog and stubs to package.

---
+++ stubs/pytket_qir/gatesets/pyqir/pyqir.pyi
@@ -0,0 +1,4 @@
+from _typeshed import Incomplete
+from pytket_qir.gatesets.base import GateSet as GateSet, OpName as OpName, OpNat as OpNat, OpSpec as OpSpec, QirGate as QirGate
+
+PYQIR_GATES: Incomplete
