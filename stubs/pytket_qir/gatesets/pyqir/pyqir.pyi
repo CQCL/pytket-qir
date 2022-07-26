6f7bdf038c5257bf3a448a7124fad1fd39074b90
Roland Guichard <roland.guichard@cambridgequantum.com>
2022-07-26 14:57:47 +0200

    Replace manually generated stubs with py.typed file.

--- stubs/pytket_qir/gatesets/pyqir/pyqir.pyi
+++
@@ -1,4 +0,0 @@
-from _typeshed import Incomplete
-from pytket_qir.gatesets.base import GateSet as GateSet, OpName as OpName, OpNat as OpNat, OpSpec as OpSpec, QirGate as QirGate
-
-PYQIR_GATES: Incomplete
