d9efcde097ae6263f936bef366e9ca030059eb5c
Roland Guichard <roland.guichard@cambridgequantum.com>
2022-11-01 14:20:56 +0000

    Black autoformatting.

--- tests/qir_test.py
+++ tests/qir_test.py
@@ -213,13 +213,19 @@ class TestQirToPytketGateTranslation:
         conditional0.op.type == OpType.Conditional
         assert conditional0.bits == list(output_register)
         assert conditional0.op.value == 1
-        assert sum([int(n) * 2**k for k, n in enumerate(conditional0.op.op.values)]) == 99
+        assert (
+            sum([int(n) * 2**k for k, n in enumerate(conditional0.op.op.values)])
+            == 99
+        )
 
         conditional1 = conditionals[1]
         conditional1.op.type == OpType.Conditional
         assert conditional1.bits == list(output_register)
         assert conditional1.op.value == 0
-        assert sum([int(n) * 2**k for k, n in enumerate(conditional1.op.op.values)]) == 22
+        assert (
+            sum([int(n) * 2**k for k, n in enumerate(conditional1.op.op.values)])
+            == 22
+        )
 
     def test_zext(self) -> None:
         zext_function_file_path = qir_files_dir / "zext.ll"
