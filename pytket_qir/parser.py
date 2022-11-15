e105aa9e07507df6129cae7c8e94820530e06c5c
Roland Guichard <roland.guichard@cambridgequantum.com>
2022-11-15 13:42:53 +0000

    Support for read_result instruction as a copy bit.

--- pytket_qir/parser.py
+++ pytket_qir/parser.py
@@ -283,9 +283,19 @@ class QirParser:
 
         for instr in instrs:
             if instr.instr.is_qis_call:  # Quantum gates.
-                op = self.get_operation(instr)
-                unitids = self.get_qubit_indices(instr.instr)
-                circuit.add_gate(op, unitids)
+                optype = self.get_optype(instr.instr)
+                if optype == OpType.CopyBits:
+                    instr = cast(QirQisCallInstr, instr)
+                    source_reg = circuit.get_c_register("c")
+                    func_arg = cast(QirResultConstant, instr.func_args[0])
+                    index = func_arg.value
+                    output_name = "%" + str(instr.output_name)
+                    target_reg = circuit.add_c_register(output_name, source_reg.size)
+                    circuit.add_c_copybits([source_reg[index]], [target_reg[index]])
+                else:
+                    op = self.get_operation(instr)
+                    unitids = self.get_qubit_indices(instr.instr)
+                    circuit.add_gate(op, unitids)
             elif instr.instr.is_qir_call:  # Setting the conditional bit for branching.
                 # Create and log register to hold the condition value.
                 output_name = "%" + str(instr.output_name)
