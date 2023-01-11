4af792304ee30b3e087900c38aa509850fe5cd2f
Roland Guichard <roland.guichard@cambridgequantum.com>
2023-01-11 11:22:06 +0000

    Tests for the new CFG analyser module.

---
+++ tests/cfg_test.py
@@ -0,0 +1,54 @@
+
+from pathlib import Path
+
+from pytket_qir.cfg import CfgAnalyser
+
+
+qir_files_dir = Path("./qir_test_files")
+
+class TestCfgAnalyser:
+
+    def test_build_cfg_for_simple_conditional(self, simple_conditional_cfg) -> None:
+       one_conditional_diamond_path = qir_files_dir / "one_conditional_diamond.bc"
+
+       cfg = CfgAnalyser(one_conditional_diamond_path)
+
+       assert cfg.cfg == simple_conditional_cfg
+       
+    def test_collapse_simple_instr_chain(self, collapsed_simple_chain_cfg) -> None:
+        collapse_simple_chain_path = qir_files_dir / "collapse_simple_instr_chain.bc"
+
+        cfg = CfgAnalyser(collapse_simple_chain_path)
+
+        cfg.collapse_blocks()
+
+        assert cfg.rewritten_cfg == collapsed_simple_chain_cfg
+        
+    def test_collapse_jump_left(self, collapsed_jump_left_cfg) -> None:
+        collapse_jump_left = qir_files_dir / "collapse_jump_left.bc"
+
+        cfg = CfgAnalyser(collapse_jump_left)
+        
+        cfg.collapse_blocks()
+
+        assert cfg.rewritten_cfg == collapsed_jump_left_cfg
+        
+    def test_collapse_jump_right(self, collapsed_jump_right_cfg) -> None:
+        collapse_jump_right = qir_files_dir / "collapse_jump_right.bc"
+
+        cfg = CfgAnalyser(collapse_jump_right)
+        
+        cfg.collapse_blocks()
+
+        assert cfg.rewritten_cfg == collapsed_jump_right_cfg
+
+        
+    def test_collapse_complex_instr_chains(self, collapsed_complex_chain) -> None:
+       collapse_complex_chain = qir_files_dir / "collapse_jump_instr.bc"
+
+       cfg = CfgAnalyser(collapse_complex_chain)
+
+       cfg.collapse_blocks()
+
+       assert cfg.rewritten_cfg == collapsed_complex_chain
+
