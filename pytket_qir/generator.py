4b8e537ace6cb9b9ddd24402c13d116afd99bfdb
Melf <cqc@melf.de>
2023-04-27 11:56:30 +0100

    [working] add rebase to circuit for command

--- pytket_qir/generator.py
+++ pytket_qir/generator.py
@@ -128,6 +128,15 @@ class QirGenerator:
             return circuit
         return None
 
+    def _rebase_op_to_gateset(self, op, args) -> Optional[Circuit]:
+        """Rebase an op to the target gateset if needed."""
+        optype = op.type
+        params = op.params        
+        circuit = Circuit(self.circuit.n_qubits, self.circuit.n_bits)
+        circuit.add_gate(optype, params, args)
+        self.rebase_to_gateset.apply(circuit)
+        return circuit
+
     def _get_optype_and_params(self, op: Op) -> Tuple[OpType, Sequence[float]]:
         optype = op.type
         params: List = []
@@ -288,9 +297,12 @@ class QirGenerator:
                 print(command.bits)
                 print(command.opgroup)
 
+                assert op.width == 1 # only ne conditional bit
+
 
                 # exit()
-                conditional_circuit = Circuit(1).H(0)
+                # conditional_circuit = Circuit(1).H(0)
+                conditional_circuit = self._rebase_op_to_gateset(op.op, command.args[op.width:])
                 condition_bit_index = command.args[0].index[0]
                 condition_name = command.args[0].reg_name
 
@@ -298,8 +310,9 @@ class QirGenerator:
 
                 #if ssa_var := self.ssa_vars.get(condition_name):
                 #    condition_ssa = ssa_var
+                # this needs to be fixed
 
-                def condition_block():
+                def condition_one_block():
                     """
                     Populate recursively the module with the contents of the conditional
                     sub-circuit when the condition is True.
@@ -317,7 +330,7 @@ class QirGenerator:
 
                 # qis = BasicQisBuilder(module.module.builder)
                 # module.qis.if_result(condition_ssa, lambda: module.qis.x(module.module.qubits[0]))
-                module.qis.if_result(condition_ssa, lambda: condition_block())
+                module.qis.if_result(condition_ssa, lambda: condition_one_block())
                 #module.module.builder.if_(
                 #    condition_ssa,
                 #    true=lambda: condition_one_block(),
