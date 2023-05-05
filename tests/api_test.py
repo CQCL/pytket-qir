3740fa329a43f17b97c2ab3d0457c8a681cbebe4
Melf <cqc@melf.de>
2023-05-05 16:50:09 +0100

    [working] add more tests

--- tests/api_test.py
+++ tests/api_test.py
@@ -45,8 +45,6 @@ def test_pytket_qir_2() -> None:
 
     result = pytket_to_qir_2(circ)
 
-    # print(result)
-
     assert result == """; ModuleID = 'Generated from input pytket circuit'
 source_filename = "Generated from input pytket circuit"
 
@@ -77,9 +75,34 @@ def test_pytket_qir_ii() -> None:
     circ.H(1)
     circ.H(2)
 
-    result = pytket_to_qir(circ)
+    result = pytket_to_qir_2(circ)
 
-    assert result == """<tket::Circuit, qubits=3, gates=3>"""
+    print(result)
+
+    assert result == """; ModuleID = 'Generated from input pytket circuit'
+source_filename = "Generated from input pytket circuit"
+
+%Qubit = type opaque
+
+define void @main() #0 {
+entry:
+  call void @__quantum__qis__h__body(%Qubit* null)
+  call void @__quantum__qis__h__body(%Qubit* inttoptr (i64 1 to %Qubit*))
+  call void @__quantum__qis__h__body(%Qubit* inttoptr (i64 2 to %Qubit*))
+  ret void
+}
+
+declare void @__quantum__qis__h__body(%Qubit*)
+
+attributes #0 = { "entry_point" "num_required_qubits"="3" "num_required_results"="3" "output_labeling_schema" "qir_profiles"="custom" }
+
+!llvm.module.flags = !{!0, !1, !2, !3}
+
+!0 = !{i32 1, !"qir_major_version", i32 1}
+!1 = !{i32 7, !"qir_minor_version", i32 0}
+!2 = !{i32 1, !"dynamic_qubit_management", i1 false}
+!3 = !{i32 1, !"dynamic_result_management", i1 false}
+"""
 
 
 def test_pytket_qir_5() -> None:
@@ -101,9 +124,75 @@ def test_pytket_qir_5() -> None:
     assert circ.n_qubits == 3
     assert circ.n_bits == 20
 
-    result = pytket_to_qir(circ)
+    result = pytket_to_qir_2(circ)
 
-    assert result == """<tket::Circuit, qubits=3, gates=7>"""
+    # print(result)
+
+    assert result == """; ModuleID = 'Generated from input pytket circuit'
+source_filename = "Generated from input pytket circuit"
+
+%Qubit = type opaque
+%Result = type opaque
+
+define void @main() #0 {
+entry:
+  %0 = call i64 @reg2var(i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false)
+  %1 = call i64 @reg2var.1(i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false)
+  %2 = or i64 %0, %1
+  call void @__quantum__qis__x__body(%Qubit* null)
+  call void @__quantum__qis__h__body(%Qubit* inttoptr (i64 1 to %Qubit*))
+  call void @__quantum__qis__h__body(%Qubit* inttoptr (i64 2 to %Qubit*))
+  call void @__quantum__qis__mz__body(%Qubit* null, %Result* inttoptr (i64 4 to %Result*))
+  %3 = call i1 @__quantum__qis__read_result__body(%Result* inttoptr (i64 4 to %Result*))
+  br i1 %3, label %then, label %else
+
+then:                                             ; preds = %entry
+  call void @__quantum__qis__z__body(%Qubit* null)
+  br label %continue
+
+else:                                             ; preds = %entry
+  br label %continue
+
+continue:                                         ; preds = %else, %then
+  %4 = icmp ne i64 %2, 2
+  br i1 %4, label %then1, label %else2
+
+then1:                                            ; preds = %continue
+  call void @__quantum__qis__z__body(%Qubit* null)
+  br label %continue3
+
+else2:                                            ; preds = %continue
+  br label %continue3
+
+continue3:                                        ; preds = %else2, %then1
+  call void @__quantum__qis__h__body(%Qubit* null)
+  ret void
+}
+
+declare i64 @reg2var(i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1)
+
+declare i64 @reg2var.1(i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1)
+
+declare void @__quantum__qis__x__body(%Qubit*)
+
+declare void @__quantum__qis__h__body(%Qubit*)
+
+declare void @__quantum__qis__mz__body(%Qubit*, %Result* writeonly) #1
+
+declare i1 @__quantum__qis__read_result__body(%Result*)
+
+declare void @__quantum__qis__z__body(%Qubit*)
+
+attributes #0 = { "entry_point" "num_required_qubits"="3" "num_required_results"="20" "output_labeling_schema" "qir_profiles"="custom" }
+attributes #1 = { "irreversible" }
+
+!llvm.module.flags = !{!0, !1, !2, !3}
+
+!0 = !{i32 1, !"qir_major_version", i32 1}
+!1 = !{i32 7, !"qir_minor_version", i32 0}
+!2 = !{i32 1, !"dynamic_qubit_management", i1 false}
+!3 = !{i32 1, !"dynamic_result_management", i1 false}
+"""
 
 
 def test_pytket_qir_3() -> None:
