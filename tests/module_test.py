746af11c24226c360f527ba0687e0c936cb93549
Melf <cqc@melf.de>
2023-05-05 13:57:12 +0100

    [working] add some parts of the module

---
+++ tests/module_test.py
@@ -0,0 +1,40 @@
+# Copyright 2020-2023 Cambridge Quantum Computing
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from pytket.extensions.qir.conversion.module import Module
+
+from pytket.circuit import Circuit, Qubit
+
+import pyqir
+
+from pytket.circuit.logic_exp import (  # type: ignore
+    BitNot,
+    if_bit,
+    if_not_bit,
+    reg_eq,
+    reg_neq,
+    reg_geq,
+    reg_gt,
+    reg_lt,
+    reg_leq,
+)
+
+
+def test_pytket_qir() -> None:
+    m = Module("modulename", 3, 1)
+    assert type(m.module) == type(pyqir.SimpleModule("name", 1, 1))
+
+
+if __name__ == "__main__":
+    test_pytket_qir()
