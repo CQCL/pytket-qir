a6ce1331c575e9e042cb965c81cc08ad129d7ef3
Melf <cqc@melf.de>
2023-05-24 13:11:53 +0100

    update

--- tests/api_test.py
+++ tests/api_test.py
@@ -15,6 +15,8 @@
 
 import pytest
 
+import pyqir
+
 from pytket.qir.conversion.api import pytket_to_qir, ReturnTypeQIR
 from pytket.circuit import Circuit  # type: ignore
 
@@ -107,6 +109,72 @@ def test_pytket_api_creg() -> None:
 
     with pytest.raises(ValueError):
         pytket_to_qir(circ)
+
+
+def test_pytket_api_creg() -> None:
+    circ = Circuit(3)
+    circ.H(0)
+
+    circ.add_c_register("c2", 100)
+
+    with pytest.raises(ValueError):
+        pytket_to_qir(circ)
+
+
+def test_pytket_qir_module() -> None:
+    circ = Circuit(3)
+    circ.H(0)
+
+    result = pytket_to_qir(
+        circ, name="test_pytket_qir", returntype=ReturnTypeQIR.STRING
+    )
+
+    result_2 = pytket_to_qir(circ, name="test_pytket_qir")
+
+    print(result)
+
+    assert (
+        result
+        == """; ModuleID = 'test_pytket_qir'
+source_filename = "test_pytket_qir"
+
+%Qubit = type opaque
+%Result = type opaque
+
+define void @main() #0 {
+entry:
+  call void @__quantum__qis__h__body(%Qubit* null)
+  ret void
+}
+
+declare i1 @read_bit_from_reg(i64, i64)
+
+declare void @set_one_bit_in_reg(i64, i64, i1)
+
+declare void @set_all_bits_in_reg(i64, i64)
+
+declare i1 @__quantum__qis__read_result__body(%Result*)
+
+declare i64 @reg2var(i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1)
+
+declare void @__quantum__rt__int_record_output(i64)
+
+declare void @__quantum__qis__h__body(%Qubit*)
+
+attributes #0 = { "entry_point" "num_required_qubits"="3" "num_required_results"="3" "output_labeling_schema" "qir_profiles"="custom" }
+
+!llvm.module.flags = !{!0, !1, !2, !3}
+
+!0 = !{i32 1, !"qir_major_version", i32 1}
+!1 = !{i32 7, !"qir_minor_version", i32 0}
+!2 = !{i32 1, !"dynamic_qubit_management", i1 false}
+!3 = !{i32 1, !"dynamic_result_management", i1 false}
+"""
+    )
+
+    bitcode = pyqir.Module.from_ir(pyqir.Context(), result).bitcode
+
+    assert bitcode == result_2
 
 
 if __name__ == "__main__":
