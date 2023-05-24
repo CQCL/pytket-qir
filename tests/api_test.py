b2b33b215ed901fb9ada54b67fe13385c45dbca9
Melf <cqc@melf.de>
2023-05-24 13:18:55 +0100

    fix entrypoint

--- tests/api_test.py
+++ tests/api_test.py
@@ -17,7 +17,7 @@ import pytest
 
 import pyqir
 
-from pytket.qir.conversion.api import pytket_to_qir, ReturnTypeQIR
+from pytket.qir.conversion.api import pytket_to_qir, pytket_to_qir_optimised, ReturnTypeQIR
 from pytket.circuit import Circuit  # type: ignore
 
 
@@ -80,6 +80,57 @@ attributes #0 = { "entry_point" "num_required_qubits"="3" "num_required_results"
 !3 = !{i32 1, !"dynamic_result_management", i1 false}
 """
     )
+
+
+def test_pytket_qir_optimised() -> None:
+    circ = Circuit(3)
+    circ.H(0)
+
+    result = pytket_to_qir_optimised(
+        circ, name="test_pytket_qir", returntype=ReturnTypeQIR.STRING
+    )
+
+    print(result)
+
+    assert (
+        result
+        == """; ModuleID = 'test_pytket_qir'
+source_filename = "test_pytket_qir"
+
+%Qubit = type opaque
+%Result = type opaque
+
+define void @main() #0 {
+entry:
+  call void @__quantum__qis__h__body(%Qubit* null)
+  ret void
+}
+
+declare i1 @read_bit_from_reg(i64, i64)
+
+declare void @set_one_bit_in_reg(i64, i64, i1)
+
+declare void @set_all_bits_in_reg(i64, i64)
+
+declare i1 @__quantum__qis__read_result__body(%Result*)
+
+declare i64 @reg2var(i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1)
+
+declare void @__quantum__rt__int_record_output(i64)
+
+declare void @__quantum__qis__h__body(%Qubit*)
+
+attributes #0 = { "EntryPoint" "num_required_qubits"="3" "num_required_results"="3" "output_labeling_schema" "qir_profiles"="custom" }
+
+!llvm.module.flags = !{!0, !1, !2, !3}
+
+!0 = !{i32 1, !"qir_major_version", i32 1}
+!1 = !{i32 7, !"qir_minor_version", i32 0}
+!2 = !{i32 1, !"dynamic_qubit_management", i1 false}
+!3 = !{i32 1, !"dynamic_result_management", i1 false}
+"""
+    )
+
 
 
 def test_pytket_api_qreg() -> None:
