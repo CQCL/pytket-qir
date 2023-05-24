a9cbb3e4046b5efb4214a8aca51f64760ee8798a
Melf <cqc@melf.de>
2023-05-24 11:02:22 +0100

    add testcase

--- tests/quantumgates_test.py
+++ tests/quantumgates_test.py
@@ -87,6 +87,70 @@ def test_pytket_qir_quantum_ii() -> None:
     print(result)
 
 
+def test_pytket_qir_quantum_iii() -> None:
+    circ = Circuit(2).H(0).CX(0, 1).measure_all()
+
+    result = pytket_to_qir(
+        circ, name="test_pytket_qir_quantum", returntype=ReturnTypeQIR.STRING
+    )
+
+    print(result)
+
+    assert (
+        result
+        == """; ModuleID = 'test_pytket_qir_quantum'
+source_filename = "test_pytket_qir_quantum"
+
+%Qubit = type opaque
+%Result = type opaque
+
+define void @main() #0 {
+entry:
+  %0 = call i64 @reg2var(i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false, i1 false)
+  call void @__quantum__qis__h__body(%Qubit* null)
+  call void @__quantum__qis__cnot__body(%Qubit* null, %Qubit* inttoptr (i64 1 to %Qubit*))
+  call void @__quantum__qis__mz__body(%Qubit* null, %Result* null)
+  %1 = call i1 @__quantum__qis__read_result__body(%Result* null)
+  call void @set_one_bit_in_reg(i64 %0, i64 0, i1 %1)
+  call void @__quantum__qis__mz__body(%Qubit* inttoptr (i64 1 to %Qubit*), %Result* inttoptr (i64 1 to %Result*))
+  %2 = call i1 @__quantum__qis__read_result__body(%Result* inttoptr (i64 1 to %Result*))
+  call void @set_one_bit_in_reg(i64 %0, i64 1, i1 %2)
+  call void @__quantum__rt__int_record_output(i64 %0)
+  ret void
+}
+
+declare i1 @read_bit_from_reg(i64, i64)
+
+declare void @set_one_bit_in_reg(i64, i64, i1)
+
+declare void @set_all_bits_in_reg(i64, i64)
+
+declare i1 @__quantum__qis__read_result__body(%Result*)
+
+declare i64 @reg2var(i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1, i1)
+
+declare void @__quantum__rt__int_record_output(i64)
+
+declare void @__quantum__qis__h__body(%Qubit*)
+
+declare void @__quantum__qis__cnot__body(%Qubit*, %Qubit*)
+
+declare void @__quantum__qis__mz__body(%Qubit*, %Result* writeonly) #1
+
+attributes #0 = { "entry_point" "num_required_qubits"="2" "num_required_results"="2" "output_labeling_schema" "qir_profiles"="custom" }
+attributes #1 = { "irreversible" }
+
+!llvm.module.flags = !{!0, !1, !2, !3}
+
+!0 = !{i32 1, !"qir_major_version", i32 1}
+!1 = !{i32 7, !"qir_minor_version", i32 0}
+!2 = !{i32 1, !"dynamic_qubit_management", i1 false}
+!3 = !{i32 1, !"dynamic_result_management", i1 false}
+"""
+    )
+
+
 if __name__ == "__main__":
     test_pytket_qir_quantum()
     test_pytket_qir_quantum_ii()
+    test_pytket_qir_quantum_iii()
