499c5ea30fb79417001e58a24fb3d4f0754f55ac
Melf <cqc@melf.de>
2024-03-19 13:41:32 +0000

    update pytket version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.24",
+        "pytket ~= 1.26",
         "pyqir == 0.10.0",
     ],
     classifiers=[
