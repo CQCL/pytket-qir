bad6da38a6b9db751aab2426bb6f9e639e4dea57
Melf <cqc@melf.de>
2024-05-17 11:58:27 +0100

    update pytket and changelog

--- docs/changelog.rst
+++ docs/changelog.rst
@@ -1,6 +1,12 @@
 Changelog
 ~~~~~~~~~
 
+0.11.0 (unreleased)
+-------------------
+
+* Updated pytket version requirement to 1.28.
+* add support for BitWiseOp.ONE
+
 0.10.1 (April 2024)
 -------------------
 
--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.27",
+        "pytket ~= 1.28",
         "pyqir == 0.10.0",
     ],
     classifiers=[
