e38e68e12c07c5d43017fb505ba4326986d4272a
Jake Arkinstall <65358059+jake-arkinstall@users.noreply.github.com>
2024-06-07 11:52:26 +0100

    Updated nix flake to reflect TKET's latest commit

--- flake.nix
+++ flake.nix
@@ -1,9 +1,16 @@
 {
   description = "Pytket QIR Extension";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  nixConfig.extra-substituters = "https://tket.cachix.org https://cache.nixos.org";
+  nixConfig.trusted-public-keys = ''
+    tket.cachix.org-1:ACdm5Zg19qPL0PpvUwTPPiIx8SEUy+D/uqa9vKJFwh0=
+    cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY=
+  '';
   inputs.flake-utils.url = "github:numtide/flake-utils";
   inputs.tket.url = "github:CQCL/tket";
-  inputs.tket.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.nixpkgs = {
+   # url = "github:nixos/nixpkgs";
+    follows = "tket/nixpkgs";
+  };
   outputs = { self, nixpkgs, flake-utils, tket }:
     flake-utils.lib.eachDefaultSystem (system:
       let
